6604758f47719d3ddbae0516c4522f3188e6a796
szaboxhead <szaboxhed@gmail.com>
2025-04-15 12:46:12 +0200

    Random koktél & alkoholos italok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Szamlalo from "./components/Szamlalo";
 import Users from "./components/Users";
+import Cocktail from "./components/Cocktail"
+import Alcohol from "./components/Alcoholism"
 
 function App() {
 
@@ -9,6 +11,11 @@ function App() {
       <h1 className="text-3xl font-bold text-center text-blue-700">React Redux</h1>
       <Szamlalo />
       <Users />
+      <div className="bg-slate-200">
+        <h1 className="text-xl font-bold text-center">Temesi Szabolcs István</h1>
+        <Cocktail/>
+        <Alcohol/>
+      </div>
     </div>
 
   )
--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterReducer from "./reducers/counterSlice";
 import usersReducer from "./reducers/userSlice";
+import cocktailReducer from "./reducers/cocktailSlice"
+import alcoholismReducer from "./reducers/alcoholismSlice"
 
 export default configureStore({
     reducer:{
         szamlalo:counterReducer,
-        users:usersReducer
+        users:usersReducer,
+        cocktail: cocktailReducer,
+        alcohol: alcoholismReducer
     }
 });
